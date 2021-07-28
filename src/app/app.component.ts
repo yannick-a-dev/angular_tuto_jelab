6e528b6b75e672d33b27fbc3268f909184272858
yannick <essolayk@yahoo.fr>
2021-07-28 23:56:51 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Bienvenu sur jeLab coder une application avec Angular 8 ';
+}
